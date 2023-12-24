c3db1d4f7e2120587e5065acaa87a9519a6ccc44
Sushant Neupane <nsushant09@gmail.com>
2023-12-24 11:08:21 +0545

    Generic Activity and Fragment

---
+++ KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/core/BaseActivity.kt
@@ -0,0 +1,43 @@
+package com.neupanesushant.kastha.core
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
+
+    protected lateinit var binding: T
+
+    @get:LayoutRes
+    protected abstract val layoutId: Int
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        setup()
+    }
+
+    private fun setup() {
+        initialize()
+        setupViews()
+        setupEventListener()
+        setupObserver()
+        setupExtras()
+    }
+
+    abstract fun setupViews()
+    abstract fun setupEventListener()
+    abstract fun setupObserver()
+
+    protected open fun initialize() {}
+    protected open fun setupExtras() {}
+
+    protected fun isPermissionGranted(grantResults: IntArray): Boolean {
+        return grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun getBinding(): T = binding
+}
