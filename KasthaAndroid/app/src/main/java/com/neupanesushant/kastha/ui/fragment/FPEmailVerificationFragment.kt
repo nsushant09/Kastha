1de19a30f7398f76481b745428fd3d15f9267410
Sushant Neupane <nsushant09@gmail.com>
2024-02-02 21:53:38 +0545

    ui : Email Verification

--- KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/ui/fragment/FPEmailVerificationFragment.kt
+++ KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/ui/fragment/FPEmailVerificationFragment.kt
@@ -1,4 +1,30 @@
 package com.neupanesushant.kastha.ui.fragment
 
-class FPEmailVerificationFragment {
+import com.neupanesushant.kastha.R
+import com.neupanesushant.kastha.appcore.RouteConfig
+import com.neupanesushant.kastha.core.AppConfig
+import com.neupanesushant.kastha.core.BaseFragment
+import com.neupanesushant.kastha.core.Router
+import com.neupanesushant.kastha.databinding.FragmentFpEmailVerificationBinding
+
+class FPEmailVerificationFragment : BaseFragment<FragmentFpEmailVerificationBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_fp_email_verification
+
+    override fun setupViews() {
+    }
+
+    override fun setupEventListener() {
+        binding.btnBack.setOnClickListener { requireActivity().onBackPressedDispatcher.onBackPressed() }
+        binding.btnVerify.setOnClickListener {
+            Router(requireActivity())
+                .route(
+                    R.id.authentication_fragment_container,
+                    AppConfig.getFragment(RouteConfig.FP_RESET_FRAGMENT)
+                )
+        }
+    }
+
+    override fun setupObserver() {
+    }
 }
