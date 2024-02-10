a2a306cee69793234663de6254a7e01648807c50
Sushant Neupane <nsushant09@gmail.com>
2024-02-10 14:27:32 +0545

    feat : Bottom Navigation View

--- KasthaAndroid/app/build.gradle.kts
+++ KasthaAndroid/app/build.gradle.kts
@@ -1,5 +1,3 @@
-import java.net.URI
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -59,4 +57,7 @@ dependencies {
 
     // Dependencies
     implementation("com.github.appsfeature:otp-view:1.0")
+
+    val koinVersion = "3.2.0"
+    implementation("io.insert-koin:koin-android:$koinVersion")
 }
