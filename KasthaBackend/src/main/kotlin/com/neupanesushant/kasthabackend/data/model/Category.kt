6dcbfcab21229be2b4dc5235a81905eea55218a6
Sushant Neupane <nsushant09@gmail.com>
2024-02-13 19:09:27 +0545

    new : Category and data

--- KasthaBackend/src/main/kotlin/com/neupanesushant/kasthabackend/data/model/Category.kt
+++ KasthaBackend/src/main/kotlin/com/neupanesushant/kasthabackend/data/model/Category.kt
@@ -1,6 +1,7 @@
 package com.neupanesushant.kasthabackend.data.model
 
 import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
@@ -17,6 +18,8 @@ data class Category(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int,
+
+    @Column(unique = true)
     val name: String,
     val imageUrl: String,
 
--- KasthaBackend/src/main/resources/data.sql
+++ KasthaBackend/src/main/resources/data.sql
@@ -1,9 +1,25 @@
+--Roles Data
 INSERT IGNORE INTO role (name)
 VALUES
 ('ADMIN'),
 ('USER');
 
+--Alignment Data
 INSERT IGNORE INTO alignment (name)
 VALUES
 ('VERTICAL'),
 ('HORIZONTAL');
+
+--Category Data
+-- Alignment Id : 1 is Vertical, 2 is Horizontal
+INSERT IGNORE INTO category (name, image_url, alignment_id)
+VALUES
+('Bed', 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600', 1),
+('Bookshelves', 'https://images.pexels.com/photos/13883390/pexels-photo-13883390.jpeg?auto=compress&cs=tinysrgb&w=600', 2),
+('Desk', 'https://images.pexels.com/photos/237436/pexels-photo-237436.jpeg?auto=compress&cs=tinysrgb&w=600', 1),
+('Lamp', 'https://images.pexels.com/photos/45072/pexels-photo-45072.jpeg?auto=compress&cs=tinysrgb&w=600', 1),
+('Mirror', 'https://images.pexels.com/photos/7045762/pexels-photo-7045762.jpeg?auto=compress&cs=tinysrgb&w=600', 2),
+('Sofa', 'https://images.pexels.com/photos/271648/pexels-photo-271648.jpeg?auto=compress&cs=tinysrgb&w=600', 1),
+('Table', 'https://images.pexels.com/photos/159839/office-home-house-desk-159839.jpeg?auto=compress&cs=tinysrgb&w=600', 1),
+('Wall Art', 'https://images.pexels.com/photos/5653734/pexels-photo-5653734.jpeg?auto=compress&cs=tinysrgb&w=600', 2),
+('Wardrobe', 'https://images.pexels.com/photos/5531540/pexels-photo-5531540.jpeg?auto=compress&cs=tinysrgb&w=600', 1);
