4f146d460ce1ae15d38848d3a6743155500dc672
Sushant Neupane <nsushant09@gmail.com>
2024-03-13 17:17:23 +0545

    refactor: AlignmentRepo

--- KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/data/repo/AlignmentRepo.kt
+++ KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/data/repo/AlignmentRepo.kt
@@ -1,9 +1,11 @@
 package com.neupanesushant.kastha.data.repo
 
+import com.neupanesushant.kastha.core.Response
 import com.neupanesushant.kastha.domain.model.Alignment
 
+
 interface AlignmentRepo {
-    suspend fun getAlignments(): List<Alignment>
-    suspend fun getAlignmentOf(id: Int): Alignment
+    suspend fun getAlignments(): Response<List<Alignment>>
+    suspend fun getAlignmentOf(id: Int): Response<Alignment>
 
 }
