a87ca400268f3767612a2a4d961a5703ab7938e2
Sushant Neupane <nsushant09@gmail.com>
2024-03-13 17:21:06 +0545

    Changes

--- KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/data/repo/AlignmentRepo.kt
+++ KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/data/repo/AlignmentRepo.kt
@@ -1,11 +1,10 @@
 package com.neupanesushant.kastha.data.repo
 
-import com.neupanesushant.kastha.core.Response
 import com.neupanesushant.kastha.domain.model.Alignment
 
 
 interface AlignmentRepo {
-    suspend fun getAlignments(): Response<List<Alignment>>
-    suspend fun getAlignmentOf(id: Int): Response<Alignment>
+    suspend fun getAlignments(): List<Alignment>
+    suspend fun getAlignmentOf(id: Int): Alignment
 
 }
--- KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/domain/repo_impl/remote/AlignmentRemoteImpl.kt
+++ KasthaAndroid/app/src/main/java/com/neupanesushant/kastha/domain/repo_impl/remote/AlignmentRemoteImpl.kt
@@ -1,14 +1,14 @@
 package com.neupanesushant.kastha.domain.repo_impl.remote
 
-import com.neupanesushant.kastha.core.requestHandler
 import com.neupanesushant.kastha.data.remote.Endpoint
 import com.neupanesushant.kastha.data.repo.AlignmentRepo
+import com.neupanesushant.kastha.domain.model.Alignment
 
 class AlignmentRemoteImpl(
     private val endpoint: Endpoint
 ) : AlignmentRepo {
-    override suspend fun getAlignments() = requestHandler { endpoint.alignments }
+    override suspend fun getAlignments()= endpoint.alignments
 
-    override suspend fun getAlignmentOf(id: Int) = requestHandler { endpoint.getAlignmentOf(id) }
+    override suspend fun getAlignmentOf(id: Int): Alignment = endpoint.getAlignmentOf(id)
 
 }
